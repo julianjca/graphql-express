8fa59d1c4064315830a0983603652461b1a0e735
Julian Anderson <julianjca11@gmail.com>
2019-11-27 09:41:13 +0700

    change app.js structure

--- app.js
+++ app.js
@@ -1,45 +1,5 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-// const faker = require('faker');
-
-const typeDefs = require('./src/graphql/schema');
-const resolvers = require('./src/graphql/resolvers/User');
-const { libs } = require('./src/graphql/resolvers');
-const db = require('./src/models');
-
-console.log(libs);
+const app = require('./src');
 
 const PORT = process.env.PORT || 8080;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: ({ req, res }) => {
-    const userId = req.headers.authorization || '';
-    return {
-      db,
-      userId,
-      res,
-      req,
-    };
-  },
-});
-
-const app = express();
-app.use(cookieParser())
-  .use(cors())
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }));
-server.applyMiddleware({ app });
-
-app.get('/logout', (req, res) => {
-  res.clearCookie('userToken');
-  res.status(200).json({
-    success: true,
-  });
-});
-
-app.listen({ port: PORT }, () => console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`));
+app.listen({ port: PORT }, () => console.log(`🚀 Server ready at http://localhost:${PORT}$/graphql`));
---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+// const faker = require('faker');
+
+const typeDefs = require('./graphql/schema');
+const resolvers = require('./graphql/resolvers/User');
+const db = require('./models');
+
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req, res }) => {
+    const userId = req.headers.authorization || '';
+    return {
+      db,
+      userId,
+      res,
+      req,
+    };
+  },
+});
+
+const app = express();
+app.use(cookieParser())
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }));
+server.applyMiddleware({ app });
+
+app.get('/logout', (req, res) => {
+  res.clearCookie('userToken');
+  res.status(200).json({
+    success: true,
+  });
+});
+
+module.exports = app;
