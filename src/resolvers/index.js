35fe65f95dbac40fb5e23a59e9b53f089c870201
Julian Anderson <julianjca11@gmail.com>
2019-11-28 11:21:10 +0700

    refactoring resolvers merging

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,16 +1,17 @@
-const { map, merge } = require('lodash');
+const { forEach, merge, isEmpty } = require('lodash');
 const libs = require('require-all')(`${__dirname}`);
 
 const resolvers = {
   Query: {},
   Mutation: {},
 };
-const testlib = map(libs, (lib, key) => (key !== 'index' ? lib.index : {}));
 
-testlib.forEach((element) => {
-  const { Query = {}, Mutation = {} } = element;
-  resolvers.Query = merge(resolvers.Query, Query);
-  resolvers.Mutation = merge(resolvers.Mutation, Mutation);
+forEach(libs, (module) => {
+  if (!isEmpty(module.index)) {
+    const { Query = {}, Mutation = {} } = module.index;
+    resolvers.Query = merge(resolvers.Query, Query);
+    resolvers.Mutation = merge(resolvers.Mutation, Mutation);
+  }
 });
 
 module.exports = resolvers;
